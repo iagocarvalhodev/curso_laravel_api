73649a262f09d88fc0f1297684d8d503c469dfe8
Iago Carvalho <iagocarvalhoweb@gmail.com>
2018-11-01 11:08:13 -0300

    Created Task Model | Controller | Route

--- database/migrations/2018_10_31_205014_create_tasks_table.php
+++ database/migrations/2018_10_31_205014_create_tasks_table.php
@@ -15,6 +15,9 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
+            $table->float('value');
+            $table->date('date_launch');
             $table->timestamps();
         });
     }
