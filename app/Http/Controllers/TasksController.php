6f3d079f6602e2cb0735d5afeff1a5d916f1662c
Iago Carvalho <iagocarvalhoweb@gmail.com>
2018-11-06 13:27:48 -0200

    Add Method Delete e Update

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -6,79 +6,55 @@ use Illuminate\Http\Request;
 
 class TasksController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
         return \App\Task::all();
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         return \App\Task::create($request->all());
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function show($id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function edit($id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function update(Request $request, $id)
     {
-        //
+        $task = \App\Task::findOrFail($id);
+
+        $task->name = $request->name;
+        $task->value = $request->value;
+        $task->date_launch = $request->date_launch;
+        $task->save();
+
+
+        return json_encode($task);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function destroy($id)
     {
-        //
+        $task = \App\Task::findOrFail($id);
+
+        $task->delete();
+
+        return 'task deletada';
     }
 }
